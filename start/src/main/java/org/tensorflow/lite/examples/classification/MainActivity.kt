83c3c754fd995cd32fe4d12d2e5694de5e9084b3
Tram Do <tram.do@3forcom.com>
2021-09-29 22:27:39 +0700

    :sparkles: implement follow doc

--- start/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.kt
+++ start/src/main/java/org/tensorflow/lite/examples/classification/MainActivity.kt
@@ -39,10 +39,14 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
+import org.tensorflow.lite.examples.classification.ml.FlowerModel
 import org.tensorflow.lite.examples.classification.ui.RecognitionAdapter
 import org.tensorflow.lite.examples.classification.util.YuvToRgbConverter
 import org.tensorflow.lite.examples.classification.viewmodel.Recognition
 import org.tensorflow.lite.examples.classification.viewmodel.RecognitionListViewModel
+import org.tensorflow.lite.gpu.CompatibilityList
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.model.Model
 import java.util.concurrent.Executors
 import kotlin.random.Random
 
@@ -209,24 +213,42 @@ class MainActivity : AppCompatActivity() {
         // TODO 1: Add class variable TensorFlow Lite Model
         // Initializing the flowerModel by lazy so that it runs in the same thread when the process
         // method is called.
+        private val flowerModel: FlowerModel by lazy {
+            // TODO 6. Optional GPU acceleration
+            val compatList = CompatibilityList()
 
-        // TODO 6. Optional GPU acceleration
-
+            val options = if (compatList.isDelegateSupportedOnThisDevice) {
+                Log.d(TAG, "This device is GPU Compatible ")
+                Model.Options.Builder().setDevice(Model.Device.GPU).build()
+            } else {
+                Log.d(TAG, "This device is GPU Incompatible ")
+                Model.Options.Builder().setNumThreads(4).build()
+            }
+            // Initialize the Flower Model
+            FlowerModel.newInstance(ctx, options)
+        }
 
         override fun analyze(imageProxy: ImageProxy) {
 
             val items = mutableListOf<Recognition>()
 
             // TODO 2: Convert Image to Bitmap then to TensorImage
-
+            val tfImage = TensorImage.fromBitmap(toBitmap(imageProxy))
             // TODO 3: Process the image using the trained model, sort and pick out the top results
+            val outputs = flowerModel.process(tfImage)
+                .probabilityAsCategoryList.apply {
+                    sortByDescending { it.score } // Sort with highest confidence first
+                }.take(MAX_RESULT_DISPLAY) // take the top results
 
             // TODO 4: Converting the top probability items into a list of recognitions
+            for (output in outputs) {
+                items.add(Recognition(output.label, output.score))
+            }
 
             // START - Placeholder code at the start of the codelab. Comment this block of code out.
-            for (i in 0 until MAX_RESULT_DISPLAY){
-                items.add(Recognition("Fake label $i", Random.nextFloat()))
-            }
+//            for (i in 0 until MAX_RESULT_DISPLAY){
+//                items.add(Recognition("Fake label $i", Random.nextFloat()))
+//            }
             // END - Placeholder code at the start of the codelab. Comment this block of code out.
 
             // Return the result
